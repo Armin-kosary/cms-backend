07ac7993b7a06e85963ce6cedbd91b964ae26a57
Armin Kosari <Armynkosari@gmail.com>
2024-07-20 15:56:55 +0430

    update

--- CMS/settings.py
+++ CMS/settings.py
@@ -158,11 +158,11 @@ AUTH_USER_MODEL = 'User.User'
 
 
 
-ALLOWED_HOSTS=['http://localhost:8000', 'http://127.0.0.1:8000']
+ALLOWED_HOSTS=['http://localhost:3000', 'http://127.0.0.1:3000']
 
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
-       'http://localhost:8000',
-       'http://127.0.0.1:8000',
+       'http://localhost:3000',
+       'http://127.0.0.1:3000',
 )
